0b1db3d62c0efbdfef0eacecdc497009b883d7cd
walid-hajhussien <eng.waleed_com@yahoo.com>
2018-11-29 17:33:14 +0200

    starting with login page

--- client/components/login.js
+++ client/components/login.js
@@ -1,23 +1,24 @@
 angular.module('app').component('login', {
   controller:function(){
-    this.name='walid'
-    this.checkLogin=function(data){
-      var obj={
-        password:data.password,
-        username:data.username
+    this.loginData = function(login){
+      var loginInfo = {
+        username: login.usernamer,
+        password: login.password
       }
-
+      console.log("Login Info Was Saved " + "Username: " + login.username + " " + "Password: " + login.password)
+      login.username = "";
+      login.password = "";
     }
-    this.valid=true
+
   },
   bindings: {},
   template:`
-<p>Welcome</p>
-<input type="text" ng-model="username"  placeholder="username...">
-{{username}}
-name {{$ctrl.name}}
-<p ng-show="$ctrl.valid">wrong password </p>
-
-
+  <form ng-submit="$ctrl.loginData(login)">
+    <h1>Login</h1>
+    <h6>Please Type Your Username and Password :</h6><br>
+    <input type="text" placeholder="Username" ng-model="login.username"/><br><br>
+    <input type="password" placeholder="Password" ng-model="login.password"/><br><br>
+    <input type="submit" value="Login" />
+  </form>
   `
 })
