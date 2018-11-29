a9b30f84190f03eb6b89b84687d51a2773457b54
walid-hajhussien <eng.waleed_com@yahoo.com>
2018-11-29 11:33:22 +0300

    (doc) add initial document

---
+++ client/components/login.js
@@ -0,0 +1,23 @@
+angular.module('app').component('login', {
+  controller:function(){
+    this.name='walid'
+    this.checkLogin=function(data){
+      var obj={
+        password:data.password,
+        username:data.username
+      }
+
+    }
+    this.valid=true
+  },
+  bindings: {},
+  template:`
+<p>Welcome</p>
+<input type="text" ng-model="username"  placeholder="username...">
+{{username}}
+name {{$ctrl.name}}
+<p ng-show="$ctrl.valid">wrong password </p>
+
+
+  `
+})
