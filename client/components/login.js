24bd3124aac44b0f0a40c5389fc0559cc5435c67
walid-hajhussien <eng.waleed_com@yahoo.com>
2018-11-30 20:50:53 +0200

    (feat) design login page with css and bootstrap

--- client/components/login.js
+++ client/components/login.js
@@ -2,7 +2,7 @@ angular.module('app').component('login', {
   controller:function(){
     this.loginData = function(login){
       var loginInfo = {
-        username: login.usernamer,
+        username: login.username,
         password: login.password
       }
       console.log("Login Info Was Saved " + "Username: " + login.username + " " + "Password: " + login.password)
@@ -13,12 +13,31 @@ angular.module('app').component('login', {
   },
   bindings: {},
   template:`
-  <form ng-submit="$ctrl.loginData(login)">
-    <h1>Login</h1>
-    <h6>Please Type Your Username and Password :</h6><br>
-    <input type="text" placeholder="Username" ng-model="login.username"/><br><br>
-    <input type="password" placeholder="Password" ng-model="login.password"/><br><br>
-    <input type="submit" value="Login" />
-  </form>
+    <section class="login-block">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 login-sec">
+            <h2 class="text-center">Login Now</h2>
+            <form class="login-form" ng-submit="$ctrl.loginData(login)">
+              <div class="form-group">
+                <label for="exampleInputEmail1" class="text-uppercase">Username</label>
+                <input type="text" class="form-control" ng-model="login.username">
+                </div>
+
+                <div class="form-group">
+                  <label for="exampleInputPassword1" class="text-uppercase">Password</label>
+                  <input type="password" class="form-control" ng-model="login.password">
+                  </div>
+                  <div class="form-check">
+                      <button type="submit" class="btn btn-login float-left">Submit</button>
+                    </div>
+                  </form>
+                  <button class="btn btn-login float-right">SignUp</button>
+                </div>
+                <div class="col-md-8 banner-sec">
+                </div>
+              </div>
+            </div>
+          </section>
   `
 })
