09c76778f096375e7be437483bad2a49ab7c435d
Ghadeer-alkhataybeh <eng.Ghadeer.alkhataybeh@gmail.com>
2018-12-01 13:03:49 +0200

    (refactor) signup page

--- client/components/signup.js
+++ client/components/signup.js
@@ -5,12 +5,13 @@ angular.module('app').component('signup', {
         var newInput ={
         firstName : input.firstName,
         lastName : input.lastName,
-        username : input.username,
         username : input.phoneNumber,
         username : input.email,
+        username : input.username,
         password : input.password,
         available : true
       }
+
       console.log("our new input" , newInput)
 
     }
@@ -18,25 +19,25 @@ angular.module('app').component('signup', {
 
   bindings: {},
   template : `
-  <div class="panel"
-     <h2>Sign Up Page</h2>
-     <p>Please enter your Information</p>
-  </div>
-  <div class="login-form">
+  <section class="signupform">
+  <div class="container">
+  <div class="signup-form">
   <div class="main-div">
-    <form id="Login" ng-submit = "$ctrl.signup(newMember)">
+    <form ng-submit = "$ctrl.signup(newMember)">
     <div class="form-group">
-      <input class="form-control type="text" placeholder="enter your First name" ng-model="newMember.firstName"/></br>
-      <input class="form-control type="text" placeholder="enter your Last name" ng-model="newMember.lastName"/></br>
-      <input class="form-control type="text" placeholder="enter your User name" ng-model="newMember.userName"/></br>
-      <input class="form-control type="number" placeholder="enter your Phone number" ng-model="newMember.phoneNumber"/></br>
-      <input class="form-control" id="inputEmail" type="email" placeholder="enter your  Email" ng-model="newMember.email"/></br>
-      <input class="form-control" id="inputPassword" type="password" placeholder="enter your Password" ng-model="newMember.password"/></br>
+      <input class="form-control type="text" placeholder="First name please...." ng-model="newMember.firstName"/></br>
+      <input class="form-control type="text" placeholder="Last name please...." ng-model="newMember.lastName"/></br>
+      <input class="form-control type="number" placeholder="Phone number...." ng-model="newMember.phoneNumber"/></br>
+      <input class="form-control" id="inputEmail" type="email" placeholder="Email ..." ng-model="newMember.email"/></br>
+      <input class="form-control type="text" placeholder="User name...." ng-model="newMember.userName"/></br>
+      <input class="form-control" id="inputPassword" type="password" placeholder="Password...." ng-model="newMember.password"/></br>
       <input class="btn btn-primary" ng-click="submit = true" type="submit" value="Sign Up"/>
       </div>
       </form>
     <p ng-show="submit">Registered Successfully</p>
   </div>
   </div>
+  </div>
+ </section>
 `
 })
