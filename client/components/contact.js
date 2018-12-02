39814287fb32fe568fc6ff7148870e5b6df50c9e
yazeedhammad <yazeedhammad91@gmail.com>
2018-12-02 11:31:19 +0200

    (feat) add contact component

---
+++ client/components/contact.js
@@ -0,0 +1,45 @@
+angular.module('app').component('contact', {
+  controller:function(){
+  //   this.loginData = function(){
+  //
+  //
+   },
+  bindings: {},
+  template:`
+    <section id="contact">
+      <div class="section-content">
+        <h1 class="section-header">Get in <span class="content-header wow fadeIn " data-wow-delay="0.2s" data-wow-duration="2s"> Touch with us</span></h1>
+        <h3>Thank you for keeping in touch with us</h3>
+      </div>
+      <div class="contact-section">
+        <div class="container">
+          <form>
+            <div class="col-md-6 form-line">
+              <div class="form-group">
+                <label for="exampleInputUsername">Username</label>
+                <input type="text" class="form-control" id="" placeholder=" Enter Username">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail">Email Address</label>
+                  <input type="email" class="form-control" id="exampleInputEmail" placeholder=" Enter Email id">
+                  </div>
+                  <div class="form-group">
+                    <label for="telephone">Mobile No.</label>
+                    <input type="tel" class="form-control" id="telephone" placeholder=" Enter 10-digit mobile no.">
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for ="description"> Message</label>
+                      <textarea  class="form-control" id="description" placeholder="Enter Your Message"></textarea>
+                    </div>
+                    <div>
+                      <button type="button" class="btn btn-default submit"><i class="fa fa-paper-plane" aria-hidden="true"></i> Send Message</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </section>
+
+  `
+})
