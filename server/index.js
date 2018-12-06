3805aeb3dff4e55b8aae88b89cbd9b96af1f4cc5
walid-hajhussien <eng.waleed_com@yahoo.com>
2018-12-06 09:30:42 +0300

    (feat) add security part

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 // Embedded below is express setup
 const express = require('express')
 var bodyParser = require('body-parser')
+var session = require('express-session');
+var cookieParser = require('cookie-parser')
+var passport=require('passport')
+var MySQLStore = require('express-mysql-session')(session);
+var dbConnection = require('../db/db.js')
 
 // NOTE: require the Routers
 var router = require('./routers/route.js');
@@ -13,6 +18,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
 //NOTE:add the static file
 app.use(express.static(__dirname + '/../client'));
 app.use(express.static(__dirname + '/../node_modules'));
@@ -24,14 +30,26 @@ app.use(bodyParser.urlencoded({
 }));
 
 
+//add session
+app.use(cookieParser());
+var sessionStore = new MySQLStore({}, dbConnection.db);
+app.use(session({
+  secret: 'huhkjhjgyftdtgi45',
+  resave: false,
+  store: sessionStore,
+  saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
-// use routers
+
+// use routerss
 app.use('/user', router);
+
+
 
 // listen for request
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Serve r is listening on port ${PORT}`);
 });
-
-
