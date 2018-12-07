758801495c4d3614c37549341ba2a3f161d0a2f3
walid-hajhussien <eng.waleed_com@yahoo.com>
2018-12-07 21:02:00 +0300

    (style) add logo

--- client/service/retriveuser.js
+++ client/service/retriveuser.js
@@ -3,7 +3,7 @@ angular.module('app').service('retriveuser', function ($http, $window) {
 
     $http({
       method: 'GET',
-      url: 'http://127.0.0.1:4000/user/retriveuser'
+      url: 'https://jood.herokuapp.com/user/retriveuser'
     }).then(function successCallback(response) {
       cb(response)
     }, function errorCallback(response) {
